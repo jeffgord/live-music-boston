1afc144cd186fa30e817af10b919d8edf0f06d01
Jeffrey Gordon <jbgordon00@gmail.com>
2023-12-16 18:44:14 -0500

    Move maintenance form definitions into separate file

--- website/static/maintenance.js
+++ website/static/maintenance.js
@@ -1,18 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Add event listeners to display modal popups onclick for various buttons on page
     var btnAddLocation = document.getElementById('btn-add-location');
     var modalAddLocation = new bootstrap.Modal(document.getElementById('modal-add-location'));
     btnAddLocation.addEventListener('click', function () {
         modalAddLocation.show();
-    });
-
-    var modalDeleteLocation = new bootstrap.Modal(document.getElementById('modal-delete-location'));
-    document.querySelectorAll('.modal-trigger-delete-location').forEach(function (button) {
-        button.addEventListener('click', function () {
-            var locationId = this.getAttribute('data-location-id');
-            document.getElementById('delete-location-id').value = locationId;
-            modalDeleteLocation.show();
-        });
     });
 
     var modalEditLocation = new bootstrap.Modal(document.getElementById('modal-edit-location'));
@@ -26,9 +16,20 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    var modalDeleteLocation = new bootstrap.Modal(document.getElementById('modal-delete-location'));
+    document.querySelectorAll('.modal-trigger-delete-location').forEach(function (button) {
+        button.addEventListener('click', function () {
+            var locationId = this.getAttribute('data-location-id');
+            document.getElementById('delete-location-id').value = locationId;
+            modalDeleteLocation.show();
+        });
+    });
+
     var modalAddVenue = new bootstrap.Modal(document.getElementById('modal-add-venue'));
     document.querySelectorAll('.modal-trigger-add-venue').forEach(function (button) {
         button.addEventListener('click', function () {
+            var locationId = this.getAttribute('data-location-id');
+            document.getElementById('add-select-venue-location').value = `${locationId}`;
             modalAddVenue.show();
         });
     });
